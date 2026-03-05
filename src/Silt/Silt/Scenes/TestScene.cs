ea37dee436da69a70f260e6ed09fecb021ea87ea
Jasper Honkasalo <japsu.honkasalo@gmail.com>
2026-03-05 17:20:49 +0200

    Update Testscene with voxel rendering

--- src/Silt/Silt/Scenes/TestScene.cs
+++ src/Silt/Silt/Scenes/TestScene.cs
@@ -2,169 +2,53 @@
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 using Silt.CameraManagement;
-using Silt.Graphics;
 using Silt.Metrics;
 using Silt.SceneManagement;
-using Silt.Utils;
-using Shader = Silt.Graphics.Shader;
-using Texture = Silt.Graphics.Texture;
+using Silt.World;
+using Silt.World.Generation;
 
 namespace Silt.Scenes;
 
 public sealed class TestScene : Scene
 {
-    private VertexArrayObject<float, uint> _vao = null!;
-    private BufferObject<float> _vbo = null!;
-    private BufferObject<uint> _ebo = null!;
-    private Texture _texture = null!;
-    private Shader _shader = null!;
-    private readonly Transform[] _transforms = new Transform[3];
+    private const int WORLD_RADIUS_CHUNKS = 2;
+    private readonly VoxelWorld _world;
+    private int _remeshIndex = 0;
     
-    private static readonly float[] _cubeVertices =
-    [
-        // X, Y, Z, U, V
-        // Z+ (back)
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-
-        // Z- (front)
-        0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-
-        // X+ (right)
-        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-
-        // X- (left)
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-        -0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-
-        // Y+ (top)
-        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-
-        // Y- (bottom)
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 1.0f
-    ];
-
-    private static readonly uint[] _cubeIndices =
-    [
-        // Z+
-        0, 1, 2,
-        0, 2, 3,
-
-        // Z-
-        4, 5, 6,
-        4, 6, 7,
-
-        // X+
-        8, 9, 10,
-        8, 10, 11,
-
-        // X-
-        12, 13, 14,
-        12, 14, 15,
-
-        // Y+
-        16, 17, 18,
-        16, 18, 19,
-
-        // Y-
-        20, 21, 22,
-        20, 22, 23
-    ];
-
-    private int _uTexture;
-    private int _uMatM;
-    private int _uMatMV;
-    private int _uMatMVP;
-
 
     public TestScene(GL gl, IWindow window) : base(gl, window)
     {
+        _world = new VoxelWorld(gl, WORLD_RADIUS_CHUNKS);
+        
+        ChunkGenerator.ConfigureNoise(0.05f);
     }
 
 
     public override void Load()
     {
         // Setup scene camera
-        CameraManager.MainCamera.Position = new Vector3(0, 0, 5);
+        const int cameraDistance = WORLD_RADIUS_CHUNKS * Chunk.SIZE * 2 * 2;
+        CameraManager.MainCamera.Position = new Vector3(-cameraDistance, cameraDistance, cameraDistance);
+        CameraManager.MainCamera.LookAt(Vector3.Zero);
         CameraManager.SetActiveController(new FreeCameraController());
-
-        // Create buffers
-        _vbo = new BufferObject<float>(GL, _cubeVertices, BufferTargetARB.ArrayBuffer);
-        _ebo = new BufferObject<uint>(GL, _cubeIndices, BufferTargetARB.ElementArrayBuffer);
-
-        // Create VAO
-        _vao = new VertexArrayObject<float, uint>(GL, _vbo, _ebo);
-        _vao.SetVertexAttributePointer(0, 3, VertexAttribPointerType.Float, 5, 0);
-        _vao.SetVertexAttributePointer(1, 2, VertexAttribPointerType.Float, 5, 3);
-
-        // Create shader
-        _shader = new Shader(GL, "base", "assets/base.vert", "assets/base.frag");
         
-        // Cache uniform locations
-        _uTexture = _shader.GetUniformLocation("u_texture");
-        _uMatM = _shader.GetUniformLocation("u_mat_m");
-        _uMatMV = _shader.GetUniformLocation("u_mat_mv");
-        _uMatMVP = _shader.GetUniformLocation("u_mat_mvp");
-
-        // Create texture
-        _texture = new Texture(GL, "assets/tex_proto_wall.png");
-
-        // Cleanup
-        _vao.Unbind();
-        _vbo.Unbind();
-        _ebo.Unbind();
-
-        // Translation
-        _transforms[0] = new Transform();
-        _transforms[0].Position = new Vector3(0.5f, 0.5f, 0f);
-
-        // Scaling
-        _transforms[1] = new Transform();
-        _transforms[1].Scale = 0.5f;
-
-        // Mixed transformation
-        _transforms[2] = new Transform();
-        _transforms[2].Position = new Vector3(-0.5f, 0.5f, 0f);
-        _transforms[2].Rotation = Quaternion.CreateFromAxisAngle(Vector3.UnitZ, 1f);
-        _transforms[2].Scale = 0.5f;
+        _world.Generate();
+        PerfMonitor.Start(100);
     }
 
 
     public override void Unload()
     {
-        _shader.Dispose();
-        _texture.Dispose();
-        _vbo.Dispose();
-        _ebo.Dispose();
-        _vao.Dispose();
+        _world.Dispose();
     }
 
 
     public override void Update(double deltaTime)
     {
-        foreach (Transform t in _transforms)
-        {
-            // Convert time to radians for rotation
-            float rotDegrees = (float)(Window.Time * 100);
-            t.Rotation = Quaternion.CreateFromAxisAngle(Vector3.UnitY, MathUtil.DegreesToRadians(rotDegrees)) *
-                         Quaternion.CreateFromAxisAngle(Vector3.UnitX, MathUtil.DegreesToRadians(rotDegrees * 0.6f));
-        }
+        _world.Update(deltaTime);
+
+        _world.ChunkManager.Chunks[_remeshIndex].UpdateMesh();
+        _remeshIndex = (_remeshIndex + 1) % _world.ChunkManager.Chunks.Length;
     }
 
 
@@ -176,27 +60,6 @@ public sealed class TestScene : Scene
 
     public override unsafe void Render(double deltaTime)
     {
-        _vao.Bind();
-
-        _shader.Use();
-        _shader.SetUniform(_uTexture, _texture, TextureUnit.Texture0);
-
-        Matrix4x4 view = CameraManager.MainCamera.GetViewMatrix();
-        Matrix4x4 projection = CameraManager.MainCamera.GetProjectionMatrix();
-
-        foreach (Transform t in _transforms)
-        {
-            Matrix4x4 mv = t.ModelMatrix * view;
-            Matrix4x4 mvp = mv * projection;
-            _shader.SetUniform(_uMatM, t.ModelMatrix);
-            _shader.SetUniform(_uMatMV, mv);
-            _shader.SetUniform(_uMatMVP, mvp);
-
-            GL.DrawElements(PrimitiveType.Triangles, (uint)_cubeIndices.Length, DrawElementsType.UnsignedInt, null);
-
-            PerfMonitor.AddDrawCalls(1);
-            PerfMonitor.AddTriangles(_cubeIndices.Length / 3);
-            PerfMonitor.AddVertices(_cubeVertices.Length / 5);
-        }
+        _world.Draw();
     }
 }
